e42fc0ca05bc3c044864e1ddfd965e3f16e324e7
Wyatt Marks <inbox@wyattmarks.com>
2022-08-16 23:10:18 -0500

    Create basic menu system to test with

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -2,18 +2,61 @@
 #include <LiquidCrystal.h>
 #include "GUI/GUI.h"
 
+#define ENC_A 34
+#define ENC_B 35
+#define SELECT 32
+#define BACK 33
+
 GUI gui;
 LiquidCrystal lcd;
 
+void menu_a() {
+	Vector<String> options;
+	options.push_back("Menu B");
+
+	Vector<void (*)()> callbacks;
+	callbacks.push_back(main_menu);
+	callbacks.push_back(menu_b);
+
+	gui.create_menu(options, callbacks);
+}
+
+void menu_b() {
+	Vector<String> options;
+	options.push_back("Menu A");
+
+	Vector<void (*)()> callbacks;
+	callbacks.push_back(main_menu);
+	callbacks.push_back(menu_a);
+
+	gui.create_menu(options, callbacks);
+}
+
 void main_menu() {
-  
+	Vector<String> options;
+	options.push_back("Menu A");
+	options.push_back("Menu B");
+
+	Vector<void (*)()> callbacks;
+	callbacks.push_back(main_menu);
+	callbacks.push_back(menu_a);
+	callbacks.push_back(menu_b);
+
+	gui.create_menu(options, callbacks);
 }
 
 void setup() {
-  lcd = LiquidCrystal(19, 23, 18, 17, 16, 13); //LCD Pins
-  gui = GUI(&lcd, 34, 35, 32, 33); //Encoder + Button Pins
+	pinMode(ENC_A, INPUT);
+	pinMode(ENC_B, INPUT);
+	pinMode(SELECT, INPUT);
+	pinMode(BACK, INPUT);
+
+	lcd = LiquidCrystal(19, 23, 18, 17, 16, 13); //LCD Pins
+	gui = GUI(&lcd, ENC_A, ENC_B, SELECT, BACK); //Encoder + Button Pins
+
+	main_menu();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	gui.process();
 }
